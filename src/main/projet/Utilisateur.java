a79562b6d77f6f6f54900f4e1a538e67fc652fa8
Autome Edwin <automeedwin@gmail.com>
2019-11-14 17:26:44 +0100

    Ajout des variables de l'objet utilisateur et de leurs getters/setters

--- src/main/projet/Utilisateur.java
+++ src/main/projet/Utilisateur.java
@@ -4,17 +4,50 @@
 package projet;
 
 /**
- * @author
+ * @author Autome Edwin
  *
  */
 public class Utilisateur {
+    
+    private String pseudo;
+    private int score;
+    
+    // GETTERS & SETTERS
+    /**
+     * @return le pseudo de l'utilisateur
+     */
+    public String getPseudo() {
+	return pseudo;
+    }   
+    
+    /**
+     * @return le score de l'utilisateur
+     */
+    public int getScore() {
+	return score;
+    }
+    
+    /**
+     * Définit le pseudo de l'utilisateur
+     * @param pseudo désiré par l'utilisateur 
+     */
+    public void setPseudo(String pseudo) {
+	this.pseudo = pseudo;
+    }
+    
+    /**
+     * Définit le score de l'utilisateur
+     * @param score que l'utilisateur obtient
+     */
+    public void setScore(int score) {
+	this.score = score;
+    }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    }
 }

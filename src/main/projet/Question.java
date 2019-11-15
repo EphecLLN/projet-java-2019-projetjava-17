dd30caa5c6ec8f3612739b97cdb428c58dffbf55
VERSCHRAEGEN Gauthier <HE201654@students.ephec.be>
2019-11-15 09:49:12 +0100

    Modif Quizz/Question

--- src/main/projet/Question.java
+++ src/main/projet/Question.java
@@ -3,18 +3,90 @@
  */
 package projet;
 
+import java.util.*;
+
 /**
- * @author
+ * @author Gauthier Verschraegen
  *
  */
 public class Question {
-
+	private String question = "";
+	private String[] reponsesF = {"","", ""};
+	private String reponseV;
+	
 	/**
-	 * @param args
+	 * @return the question
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String getQuestion() {
+		return question;
 	}
-
+	/**
+	 * @param question the question to set
+	 */
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	/**
+	 * @return the reponsesF
+	 */
+	public String[] getReponsesF() {
+		return reponsesF;
+	}
+	/**
+	 * @param reponsesF the reponsesF to set
+	 */
+	public void setReponsesF(String[] reponsesF) {
+		this.reponsesF = reponsesF;
+	}
+	/**
+	 * @return the reponseV
+	 */
+	public String getReponseV() {
+		return reponseV;
+	}
+	/**
+	 * @param reponseV the reponseV to set
+	 */
+	public void setReponseV(String reponseV) {
+		this.reponseV = reponseV;
+	}	
+	
+	/**
+	 * 
+	 */
+	public void genererQuestion() {
+		this.setQuestion("Comment je m'appelle?");
+		System.out.println(getQuestion());
+		Scanner scanner = new Scanner(System.in);
+		String inputString = "";
+		while ((inputString != "Carre") || (inputString != "Cash")) {
+			System.out.println("Comment souhaitez-vous Répondre ?");
+			inputString = scanner.nextLine();
+		}
+		scanner.close();
+		genererReponse(inputString);
+		
+	}
+	public void genererReponse(String choix) {
+		int[] flags = {0,0,0,0};
+		setReponseV("Moi");
+		String[] rep = {"Lui", "Il", "Toi"};
+		Random rand = new Random();
+		int num;
+		if (choix == "Carre") {
+			for (int i = 0;i<(rep.length+1);i++) {
+				num = rand.nextInt((rep.length+1));
+				System.out.println(num);
+			}
+		}
+	}
+	public int reponseCash() {
+		return 3;
+	}
+	public int reponseCarre() {
+		return 1;
+	}
+	public boolean checkReponse(String rep) {
+		return true;
+	}
 }
--- src/main/projet/Quizz.java
+++ src/main/projet/Quizz.java
@@ -4,17 +4,81 @@
 package projet;
 
 /**
- * @author
+ * @author obi-o
  *
  */
 public class Quizz {
+
+	private static int compteurQuestion = 0;
+	private static int nombreQuestionRatees = 0;
+	
+	private static String theme = "";
+	private static String difficulte = "";
 	
 	/**
-	 * @param args
+	 * @return the compteurQuestion
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int getCompteurQuestion() {
+		return compteurQuestion;
 	}
 
+	/**
+	 * @param compteurQuestion the compteurQuestion to set
+	 */
+	public void setCompteurQuestion(int compteurQuestion) {
+		this.compteurQuestion = compteurQuestion;
+	}
+
+	/**
+	 * @return the nombreQuestionRatees
+	 */
+	public int getNombreQuestionRatees() {
+		return nombreQuestionRatees;
+	}
+
+	/**
+	 * @param nombreQuestionRatees the nombreQuestionRatees to set
+	 */
+	public void setNombreQuestionRatees(int nombreQuestionRatees) {
+		this.nombreQuestionRatees = nombreQuestionRatees;
+	}
+
+	/**
+	 * @return the theme
+	 */
+	public String getTheme() {
+		return theme;
+	}
+
+	/**
+	 * @param theme the theme to set
+	 */
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+
+	/**
+	 * @return the difficulte
+	 */
+	public String getDifficulte() {
+		return difficulte;
+	}
+
+	/**
+	 * @param difficulte the difficulte to set
+	 */
+	public void setDifficulte(String difficulte) {
+		this.difficulte = difficulte;
+	}
+
+	public void questionner() {
+		Question maQuestion = new Question();
+		for (setCompteurQuestion(0);getCompteurQuestion()<10;setCompteurQuestion((getCompteurQuestion()+1))) {
+			maQuestion.genererQuestion();
+		}
+	}
+	public static void main (String[] args){
+		Quizz quizz = new Quizz();
+		quizz.questionner();
+		}
 }

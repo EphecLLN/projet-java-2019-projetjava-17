9789ed99ca0fbcb9d9c5302dcf054e10742099a3
Florent Theys <f.theys@students.ephec.be>
2019-12-16 22:41:43 +0100

    auto clean up

--- src/projetQuizz/modele/Reponse.java
+++ src/projetQuizz/modele/Reponse.java
@@ -2,38 +2,38 @@ package projetQuizz.modele;
 
 
 public class Reponse {
-    private boolean estBonneReponse;
-    private String reponse;
-    private int id;
+	private boolean estBonneReponse;
+	private String reponse;
+	private int id;
 
-    /**
-     * Constructeur de la classe Reponse. 
-     */
-    Reponse(int id, String reponse) {
-        this(id, reponse, false);
-    }
+	/**
+	 * Constructeur de la classe Reponse.
+	 */
+	Reponse(int id, String reponse) {
+		this(id, reponse, false);
+	}
 
-    /**
-     * Autre constructeur de la classe Reponse. 
-     */
-    Reponse(int id, String reponse, boolean estBonneReponse) {
-        this.id = id;
-        this.reponse = reponse;
-        this.estBonneReponse = estBonneReponse;
-    }
+	/**
+	 * Autre constructeur de la classe Reponse.
+	 */
+	Reponse(int id, String reponse, boolean estBonneReponse) {
+		this.id = id;
+		this.reponse = reponse;
+		this.estBonneReponse = estBonneReponse;
+	}
 
-    /**
-     * Getter de estBonneReponse.
-     */
-    public boolean getEstBonneReponse() {
-        return estBonneReponse;
-    }
+	/**
+	 * Getter de estBonneReponse.
+	 */
+	public boolean getEstBonneReponse() {
+		return estBonneReponse;
+	}
 
-    /**
-     * Getter de reponse.
-     */
-    public String getReponse() {
-        return reponse;
-    }
+	/**
+	 * Getter de reponse.
+	 */
+	public String getReponse() {
+		return reponse;
+	}
 
 }
--- src/projetQuizz/modele/Resultat.java
+++ src/projetQuizz/modele/Resultat.java
@@ -4,43 +4,43 @@ import java.util.ArrayList;
 
 public class Resultat {
 
-    private Question question;
-    private int score;
-    private boolean estBonneReponse;
+	private Question question;
+	private int score;
+	private boolean estBonneReponse;
 
-    /**
-     * Constructeur de la classe Resultat.
-     */
-    public Resultat(Question question, int score, boolean estBonneReponse) {
-        this.question = question;
-        this.score = score;
-        this.estBonneReponse = estBonneReponse;
-    }
-    
-    /**
-     * Getter de bonneReponse.
-     */
-    public Reponse getBonneReponse() {
-        ArrayList<Reponse> reponses = question.getReponses();
-        for (int i=0; i<reponses.size(); i++) {
-            if (reponses.get(i).getEstBonneReponse()) {
-                return reponses.get(i);
-            }
-        }
-        return null;
-    }
+	/**
+	 * Constructeur de la classe Resultat.
+	 */
+	public Resultat(Question question, int score, boolean estBonneReponse) {
+		this.question = question;
+		this.score = score;
+		this.estBonneReponse = estBonneReponse;
+	}
 
-    /**
-     * Getter de score.
-     */
-    public int getScore() {
-        return score;
-    }
+	/**
+	 * Getter de bonneReponse.
+	 */
+	public Reponse getBonneReponse() {
+		ArrayList<Reponse> reponses = question.getReponses();
+		for (int i=0; i<reponses.size(); i++) {
+			if (reponses.get(i).getEstBonneReponse()) {
+				return reponses.get(i);
+			}
+		}
+		return null;
+	}
 
-    /**
-     * Getter de estBonneReponse.
-     */
-    public boolean getEstBonneReponse() {
-        return estBonneReponse;
-    }
+	/**
+	 * Getter de score.
+	 */
+	public int getScore() {
+		return score;
+	}
+
+	/**
+	 * Getter de estBonneReponse.
+	 */
+	public boolean getEstBonneReponse() {
+		return estBonneReponse;
+	}
 }
